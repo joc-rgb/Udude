8a95cadb2b03c719dafdd246cd5e28fd50021f73
jocxy <jocelinxy@gmail.com>
2023-01-30 20:18:42 +0800

    Context

---
+++ src/context/UDudeContext.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+
+export const UDudeContext = createContext();
+
+export const UDudeProvider = ({ children }) => {
+	const [selected, setSelected] = useState("Home");
+	const [selectedCategory, setSelectedCategory] = useState(null);
+	const [drawerOpen, setDrawerOpen] = useState(false);
+
+	const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+	const options = {
+		params: {
+			maxResults: 30,
+		},
+
+		headers: {
+			"X-RapidAPI-Key": import.meta.env.VITE_RAPID_KEY,
+			"X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+		},
+	};
+
+	const fetchYoutube = async (url) => {
+		const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+		return data;
+	};
+
+	return (
+		<UDudeContext.Provider
+			value={{
+				selected,
+				setSelected,
+				selectedCategory,
+				setSelectedCategory,
+				fetchYoutube,
+				drawerOpen,
+				setDrawerOpen,
+			}}
+		>
+			{children}
+		</UDudeContext.Provider>
+	);
+};
---
+++ src/utils/constant.js
@@ -0,0 +1,19 @@
+import {
+	BsCodeSlash,
+	BsLightbulbFill,
+	BsMusicNoteBeamed,
+	BsNewspaper,
+} from "react-icons/bs";
+import { FaBtc, FaDumbbell, FaTshirt } from "react-icons/fa";
+import { MdFaceRetouchingNatural, MdTheaterComedy } from "react-icons/md";
+export const categories = [
+	{ name: "Coding", icon: BsCodeSlash },
+	{ name: "Music", icon: BsMusicNoteBeamed },
+	{ name: "Education", icon: BsLightbulbFill },
+	{ name: "News", icon: BsNewspaper },
+	{ name: "Sport", icon: FaDumbbell },
+	{ name: "Fashion", icon: FaTshirt },
+	{ name: "Beauty", icon: MdFaceRetouchingNatural },
+	{ name: "Comedy", icon: MdTheaterComedy },
+	{ name: "Blockchain", icon: FaBtc },
+];
